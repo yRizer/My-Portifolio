955869551cd457da53151450d8a49e97b084dd8f
yRizer <taynan28112003@gmail.com>
2024-12-10 11:12:10 -0300

    Nova versão do side-bar

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <main class="main">
+    <SideBar />
+    
+  </main>
+</template>
+
+<script>
+import SideBar from './components/side-bar/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style>
+:root {
+  --seasalt: #f8f9faff;
+  --antiflash-white: #e9ecefff;
+  --platinum: #dee2e6ff;
+  --french-gray: #ced4daff;
+  --french-gray-2: #adb5bdff;
+  --slate-gray: #6c757dff;
+  --outer-space: #495057ff;
+  --onyx: #343a40ff;
+  --eerie-black: #212529ff;
+  --semi-black:#0e0e0e;
+  --black: #050505;
+}
+</style>
+
+<style scoped>
+
+main {
+  background-color: var(--black);
+  min-width: 100%;
+  min-height: 100vh;
+}
+
+</style>
---
+++ src/components/side-bar/SideBar.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="side-bar">
+        <div class="indexes-menu">
+            <div class="indexes-header"></div>
+        </div>
+        <div class="right-menu">
+            <ul class="list-options">
+                <li class="option-menu"></li>
+                <li class="option-menu"></li>
+                <li class="option-menu"></li>
+                <li class="option-menu"></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SideBar',
+}
+
+mounted(){
+    var allOptionMenu = document.querySelectorAll('.option-menu')
+    console.log(allOptionMenu);
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.side-bar {
+    width: 350px;
+    height: 100vh;
+    display: flex;
+    background-color: var(--semi-black);
+}
+
+.right-menu {
+    width: 50px;
+    height: 100%;
+    right: 0;
+    background-color: #ff00001c;
+}
+
+.indexes-menu {
+    background-color: #ffff001e;
+    height: 100%;
+    width: 300px;
+}
+
+.indexes-header {
+    background-color: rgba(0, 0, 255, 0.13);
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    position: relative;
+}
+
+.list-options {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    margin: 0;
+    padding: 0;
+    gap: 5px;
+    height: 100%;
+}
+
+.list-options li {
+    list-style: none;
+}
+
+.option-menu {
+    width: 40px;
+    height: 40px;
+    background-color: rgba(51, 255, 0, 0.603);
+}
+</style>
